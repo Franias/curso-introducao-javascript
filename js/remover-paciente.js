d44653f0765529089284aa56db03b845ec4df435
franias <franciellipdias@gmail.com>
2019-08-05 18:20:43 -0300

    Adiciona funcao de remover pacient

---
+++ js/remover-paciente.js
@@ -0,0 +1,7 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+var tabela = document.querySelectorAll("table");
+
+tabela.addEventListener("dblclick", function() {
+        this.child .remove();
+});
